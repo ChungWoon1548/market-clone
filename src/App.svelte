bc94f27af4a59ea7616f6d2b3ae467614a73de33
ChungWoon1548 <kcw1548@naver.com>
2024-02-02 14:19:40 +0900

    유저 인증까지 파이어페이스로 구현

--- src/App.svelte
+++ src/App.svelte
@@ -5,19 +5,51 @@
   import NotFound from "./pages/NotFound.svelte";
   import Signup from "./pages/Signup.svelte";
   import Write from "./pages/Write.svelte";
+  import MyPage from "./pages/MyPage.svelte";
   import Router from "svelte-spa-router";
   import "./css/style1.css";
+  import { user$ } from "./store";
+  import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { onMount } from "svelte";
+  import Loading from "./pages/Loading.svelte";
+
+  let isLoading = true;
+
+  const auth = getAuth();
+
+  const checkLogin = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return (isLoading = false);
+
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
 
   const routes = {
     "/": Main,
-    "/login": Login,
     "/signup": Signup,
     "/write": Write,
+    "/my": MyPage,
+    "/load": Loading,
     "*": NotFound,
   };
+
+  onMount(() => checkLogin());
 </script>
 
+{#if isLoading}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
 <!-- HTML 작성 -->
-<Router {routes} />
-
 <!-- CSS 작성 -->
